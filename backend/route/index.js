690a35d90f9d2b4cff4a76329f89518d12441dc5
Aniketraj19 <aniketraj19verma@gmail.com>
2025-02-28 18:14:12 +0530

    zod added

--- backend/route/index.js
+++ backend/route/index.js
@@ -5,9 +5,18 @@ route.use(express.json());
 const jwt = require("jsonwebtoken");
 const zod = require("zod");
 
+const UserValid = zod.object({
+  username: zod.string(),
+  password: zod.string().min(6),
+});
+
 route.get("/", async (req, res) => {
   const response = await Todos.find({});
   res.json(response);
+});
+
+route.post("/signup", async (req, res) => {
+  const validity = UserValid.safeParse(req.body);
 });
 
 route.listen(3000);
